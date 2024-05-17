5f36881db4840249a1cbe5867ab988d421aea408
Venkat-015 <venkat15003sridhar@gmail.com>
2024-05-17 16:16:17 +0530

    graphql

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,13 +1,18 @@
-import ProductCard from '../product-card/product-card.component';
 
+import ProductCard from '../product-card/product-card.component';
+import Spinner from "../../components/spinner/spinner.component"
 import {
   CategoryPreviewContainer,
   Title,
   Preview,
 } from './category-preview.styles';
 
-const CategoryPreview = ({ title, products }) => {
-  return (
+import { Fragment } from 'react';
+
+const CategoryPreview = ({ title, products,loading}) => {
+  return (<Fragment>
+      {
+    loading? (<Spinner />):(<Fragment>
     <CategoryPreviewContainer>
       <h2>
         <Title to={title}>{title.toUpperCase()}</Title>
@@ -19,7 +24,7 @@ const CategoryPreview = ({ title, products }) => {
             <ProductCard key={product.id} product={product} />
           ))}
       </Preview>
-    </CategoryPreviewContainer>
+    </CategoryPreviewContainer></Fragment>)}</Fragment>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
-
+import { ApolloClient,InMemoryCache,ApolloProvider } from '@apollo/client';
 import './index.scss';
-
+const client=new ApolloClient(
+  {
+    uri:'https://crwn-clothing.com/',
+    cache:new InMemoryCache()
+  }
+)
 const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <BrowserRouter>
       <UserProvider>
         <CategoriesProvider>
@@ -22,6 +28,7 @@ render(
         </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
   rootElement
 );
